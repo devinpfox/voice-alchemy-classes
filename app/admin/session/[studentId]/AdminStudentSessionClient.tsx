8fdf7dddb7a9d476c8d1df093fcaa5f51c1029ed
Devin <devinfoxbusiness@gmail.com>
2025-09-18 18:15:06 -0700

    warm ups 2

--- app/admin/session/[studentId]/AdminStudentSessionClient.tsx
+++ app/admin/session/[studentId]/AdminStudentSessionClient.tsx
@@ -2,12 +2,14 @@
 import { useParams } from 'next/navigation'
 import SessionView from '../../../../components/SessionView'
 import TunerButton from '@/components/TunerButton'
+import WarmUpButton from "@/components/WarmUpButton";
 
 export default function AdminStudentSessionPage() {
   const { studentId } = useParams<{ studentId: string }>()
   return (
     <>
       <SessionView studentId={studentId} isAdmin />
+      <WarmUpButton />
       <TunerButton /> 
     </>
   )
